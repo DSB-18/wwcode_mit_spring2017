60208b8b40e737f83b529a4a5a115cf783daa344
DSB-18 <boboshko.dmitriy@gmail.com>
2017-04-13 22:15:03 +0300

    Add files via upload
    pset0 code added

---
+++ pset0.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 22:12:37 2017
+
+@author: Дима
+"""
+
+import math
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+print("x**y =", x**y)
+print("log(x) =", int(math.log2(x)))
